5ed2e6d4bc4787e546d8e92a78cda5a8aa43ba93
Laura <zazenergy@gmail.com>
2012-04-24 15:31:04 -0700

    Updated Geary, Contribute and Shotwell Install page

--- scripts/install.js
+++ scripts/install.js
@@ -1,41 +1,29 @@
 $(document).ready(function() { 
   $('input:radio:last-child').attr('checked',true);
-  $("input[name$='group1']").click(function(){
-  	$("input[name$='group1'] p").remove();
+  	$("input[name$='group1']").click(function(){
+  		$("input[name$='group1'] p").remove();
   	
-  var radio_value = $(this).val();
- 
-  if(radio_value=='opt1') {
-
-    $("#ubuntu").show();
-     $("#fedora").hide();
-     $("#suse").hide();
-     $("#other").hide();
-  }
-  
-  
-  else if(radio_value=='opt2') {
-   $("#fedora").show();
-     $("#ubuntu").hide();
-     $("#suse").hide();
-     $("#other").hide();
-   }
-   
-  else if(radio_value=='opt3') {
-   $("#suse").show();
-   	$("#ubuntu").hide();
-   	$("#fedora").hide();
-   	$("#other").hide();
-   	
-   }
-   
-  else if(radio_value=='opt4') {
-   $("#other").show();
-    $("#ubuntu").hide();
-    $("#fedora").hide();
-    $("#suse").hide();
-   }
-   
+		  var radio_value = $(this).val();
+		 
+		  if(radio_value=='opt1') {
+		
+		     $("#ubuntu").show();
+		     $("#fedora").hide();
+		     $("#other").hide();
+		  }
+		  
+		  
+		  else if(radio_value=='opt2') {
+		   	 $("#fedora").show();
+		     $("#ubuntu").hide();
+		     $("#other").hide();
+		   }
+		   
+		  else if(radio_value=='opt3') {
+		    $("#other").show();
+		    $("#ubuntu").hide();
+		    $("#fedora").hide();
+		   }
   }); 
 });
 
